c7f6938b506fe38517b2de4377c2578a52ce9254
Josee <info@joseewouters.nl>
2018-02-17 15:12:31 +0100

    Add button and piezo

--- pomodoro.ino
+++ pomodoro.ino
@@ -2,7 +2,7 @@
 
 #define NUM_PIXELS 12 // amount of LED
 
-unsigned long interval = 60000;  // the time we need to wait
+unsigned long interval = 3000;  // the time we need to wait
 unsigned long previousMillis = 0;
 
 uint32_t currentColor;// current Color in case we need it
@@ -11,7 +11,13 @@ uint16_t currentPixel = 0;// what pixel are we operating on
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, 2, NEO_GRB + NEO_KHZ800);
 
+int piezoPin = 6;
+
+int startButton = 12;
+int buttonState = 0;
+
 void setup() {
+  pinMode(startButton, INPUT_PULLUP);
   currentColor = strip.Color(0,255,0);
   currentPixel = 0;
   strip.begin();
@@ -19,22 +25,26 @@ void setup() {
   strip.show(); // Initialize all pixels to 'off'
 }
 
-// Basic idea. You could reorg and pass pixel index and color as a function - or you could go through a gradient of colors.
 void loop(){
-if ((unsigned long)(millis() - previousMillis) >= interval) {
-  previousMillis = millis();
-  colorWipe();
- }
+  
+buttonState = digitalRead(startButton);
+  if(buttonState == HIGH) {
+    pomodoro();
+  }
 }
 
-void colorWipe(){
-  strip.setPixelColor(currentPixel,currentColor);
-  strip.show();
-  currentPixel++;
-  if(currentPixel > NUM_PIXELS){
-    timesUp();
-  }
-  almostTime();
+void pomodoro(){
+  if ((unsigned long)(millis() - previousMillis) >= interval) {
+    previousMillis = millis();
+    strip.setPixelColor(currentPixel,currentColor);
+    strip.show();
+    currentPixel++;
+    tone(piezoPin, 100, 200);
+    if(currentPixel > NUM_PIXELS){
+      timesUp();
+    }
+    almostTime();
+    }
 }
 
 void almostTime() {
@@ -45,7 +55,14 @@ void almostTime() {
 
 void timesUp() {
     for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i,0,0,0); //blue
+    strip.setPixelColor(i,0,0,0);
   }
   strip.show();
+  tone(piezoPin, 500, 200);
+  delay(300);
+  tone(piezoPin, 700, 300);
+  delay(300);
+  tone(piezoPin, 900, 700);
+  delay(700);
+  exit(0);
 }
