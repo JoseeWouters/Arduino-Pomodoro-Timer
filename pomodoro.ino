5463b8451330496b367c602766962a7632062684
Josee <info@joseewouters.nl>
2018-02-24 13:29:47 +0100

    Start and restart with button
    Used a push button with an LED. While timer is running, LED is on.

--- pomodoro.ino
+++ pomodoro.ino
@@ -2,7 +2,7 @@
 
 #define NUM_PIXELS 12 // amount of LED
 
-unsigned long interval = 3000;  // the time we need to wait
+unsigned long interval = 60000;  // the time we need to wait
 unsigned long previousMillis = 0;
 
 uint32_t currentColor;// current Color in case we need it
@@ -11,13 +11,19 @@ uint16_t currentPixel = 0;// what pixel are we operating on
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_PIXELS, 2, NEO_GRB + NEO_KHZ800);
 
-int piezoPin = 6;
+int ledButton = 11;
 
 int startButton = 12;
-int buttonState = 0;
+int buttonState = LOW;
+int reading;
+int previous = HIGH;
+
+long time = 0;
+long debounce = 500;
 
 void setup() {
   pinMode(startButton, INPUT_PULLUP);
+  pinMode(ledButton, OUTPUT);
   currentColor = strip.Color(0,255,0);
   currentPixel = 0;
   strip.begin();
@@ -26,31 +32,44 @@ void setup() {
 }
 
 void loop(){
-  
-buttonState = digitalRead(startButton);
-  if(buttonState == HIGH) {
+
+  reading = digitalRead(startButton);
+
+  if (reading == HIGH && previous == LOW && (millis() - time) > debounce) {
+    if (buttonState == HIGH) {
+      buttonState = LOW;
+    } else if (buttonState == LOW) {
+      buttonState = HIGH;
+    }
+    time = millis();
+  }
+  previous = reading;
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledButton, HIGH);
     pomodoro();
+    timesUp();
+    buttonState = LOW;
+  } 
+  else if (buttonState == LOW) {
+    digitalWrite(ledButton, LOW);
   }
 }
 
 void pomodoro(){
-  if ((unsigned long)(millis() - previousMillis) >= interval) {
-    previousMillis = millis();
+  
+  unsigned long currentMillis = millis(); // grab current time
+
+  for (uint16_t led = 0; led < strip.numPixels(); led++) {
     strip.setPixelColor(currentPixel,currentColor);
     strip.show();
+    while (millis() < currentMillis + interval) {
+      // do nothing!
+    }
     currentPixel++;
-    tone(piezoPin, 100, 200);
-    if(currentPixel > NUM_PIXELS){
-      timesUp();
-    }
-    almostTime();
-    }
-}
-
-void almostTime() {
-  if(currentPixel >= 9) {
-    currentColor = strip.Color(255,0,0);
+    currentMillis = millis();
   }
+  currentPixel = 0;
 }
 
 void timesUp() {
@@ -58,11 +77,4 @@ void timesUp() {
     strip.setPixelColor(i,0,0,0);
   }
   strip.show();
-  tone(piezoPin, 500, 200);
-  delay(300);
-  tone(piezoPin, 700, 300);
-  delay(300);
-  tone(piezoPin, 900, 700);
-  delay(700);
-  exit(0);
 }
